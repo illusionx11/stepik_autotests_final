b43e05e4d5003177857fd5e5a8fbbab0e4ee6c94
Ilya <izalamaev@upbeatsoft.ru>
2025-01-12 18:13:29 +0600

    is_element_present method in base class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,19 @@
 from selenium.webdriver import Chrome, Firefox, Safari, Edge, ChromiumEdge, Ie
+from selenium.common.exceptions import *
 
 class BasePage():
     
-    def __init__(self, browser: Chrome | Firefox | Safari | Edge | ChromiumEdge | Ie, url: str):
+    def __init__(self, browser: Chrome | Firefox | Safari | Edge | ChromiumEdge | Ie, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
         
     def open(self):
         self.browser.get(self.url)
+        
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
